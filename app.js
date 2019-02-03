2e233cbb08f4e7bf2e9f4f6f747cbb4ebf73c70c
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-03 17:18:50 +0800

    setting up basic tests and connecting to db

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 
+const mongoose = require('./server/db/mongoose');
+
+//Middleware
 app.use(express.json());
 const port = process.env.PORT || 8081;
 
