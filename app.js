0158915e236d4c54e6200f917190ee3eb9b7ba67
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-16 20:45:13 +0800

    creating past show api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const upcomingShowsAPI = require('./server/routes/upcomingShows');
+const pastShowsAPI = require('./server/routes/pastShows');
 
 const mongoose = require('./server/db/mongoose');
 
@@ -10,6 +11,7 @@ app.use(express.json());
 const port = process.env.PORT || 8081;
 
 app.use('/api/upcomingshows', upcomingShowsAPI);
+app.use('/api/pastshows', pastShowsAPI);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
