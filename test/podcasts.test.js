001dc1f1f33a07a6af44439258db099e8c4cdac1
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-18 15:20:27 +0800

    fixing podcats.test.js logic

--- test/podcasts.test.js
+++ test/podcasts.test.js
@@ -3,6 +3,11 @@ const should = require('chai').should();
 const request = require('supertest');
 const app = require('../app');
 
+// note that I need to hardcode the _id for the POST so I create a new post with a given id.
+// then I can use that hardcoded _id for the PUT & for the DELETE.
+// this means that I am deleting the test object that I create.
+// additionally I need to run DELETE after PUT, so that the final step of the test is DELETE
+
 describe('Checks Podcasts CRUD functions', () => {
   it('GET /api/podcasts checks get all podcasts', done => {
     request(app)
@@ -57,20 +62,7 @@ describe('Checks Podcasts CRUD functions', () => {
       .expect(500, done);
   });
 
-  it('DELETE /api/podcasts | deletes an existing podcast', function(done) {
-    request(app)
-      .delete('/api/podcasts/5c6939db70b3037c9a5d9fa8')
-      .expect('Content-Type', /json/, done);
-  });
-
-  it('DELETE /api/podcasts | fails to deletes an existing podcast-podcast not found', function(done) {
-    request(app)
-      .delete('/api/podcasts/non_existant_id')
-      .expect('Content-Type', /text/)
-      .expect(500, done);
-  });
-
-  let successful_Put_Podcast_Data = {
+  let dummy_Put_Podcast_Data = {
     podcastCoverImageLink: 'dummy_update',
     podcastName: 'dummy_update',
     podcastBlurb: 'dummy_update',
@@ -80,23 +72,28 @@ describe('Checks Podcasts CRUD functions', () => {
 
   it('PUT /api/podcasts | updates an existing podcast', function(done) {
     request(app)
-      .put('/api/podcasts/5c6939db70b3037c9a5d9fa')
-      .send(successful_Put_Podcast_Data)
-      .expect('Content-Type', /text/, done);
+      .put('/api/podcasts/5c6939db70b3037c9a5d9fa8')
+      .send(dummy_Put_Podcast_Data)
+      .expect('Content-Type', /json/, done);
   });
-
-  let failed_Put_Podcast_Data = {
-    // podcastCoverImageLink: 'dummy_update',
-    podcastName: 'dummy_update',
-    podcastBlurb: 'dummy_update',
-    podcastShowNotes: 'dummy_update',
-    podcastEmbedLink: 'dummy_update'
-  };
 
   it('PUT /api/podcasts | fails to update an existing podcast', function(done) {
     request(app)
-      .put('/api/podcasts/5c6939db70b3037c9a5d9fa')
-      .send(successful_Put_Podcast_Data)
+      .put('/api/podcasts/not_an_id')
+      .send(dummy_Put_Podcast_Data)
+      .expect('Content-Type', /text/)
+      .expect(500, done);
+  });
+
+  it('DELETE /api/podcasts | deletes an existing podcast', function(done) {
+    request(app)
+      .delete('/api/podcasts/5c6939db70b3037c9a5d9fa8')
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('DELETE /api/podcasts | fails to deletes an existing podcast-podcast not found', function(done) {
+    request(app)
+      .delete('/api/podcasts/non_existant_id')
       .expect('Content-Type', /text/)
       .expect(500, done);
   });
