e96e1c27f54598ce04ac4c53a30a05af41d77787
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-18 16:32:41 +0800

    cleaning up issue3

--- server/routes/storySubmission.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const storySubmissionController = require('../controllers/storySubmissionController');
-
-router.post('/', storySubmissionController.postStorySubmission);
-router.get('/', storySubmissionController.getStorySubmission);
-
-module.exports = router;
