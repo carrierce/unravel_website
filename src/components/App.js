cb044331080d04424fc8fe08c61a6e22f4c1d4e6
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-19 10:34:22 +0800

    adding semantic-ui and setting up a child component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import UpcomingShows from './UpcomingShows';
 
 const App = () => {
-  return <div>Hi there! </div>;
+  return <UpcomingShows />;
 };
 
 export default App;
