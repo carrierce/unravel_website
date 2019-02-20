57cb1211c5ebde0cb5519f4811456bf10286c994
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-20 15:10:47 +0800

    got delete working in upcoming shows

--- src/components/App.js
+++ src/components/App.js
@@ -16,26 +16,32 @@ class App extends React.Component {
   componentDidMount() {
     // this.getStoryDataFromDb();
     this.getUpcomingShowsFromDb();
+    // this.deleteUpcomingShow('XXXX');
   }
 
   getStoryDataFromDb = () => {
     axios.get('http://localhost:8000/api/storysubmission/').then(response => {
-      console.log(typeof response.data);
+      // console.log(typeof response.data);
       this.setState({
         storysubmission: response.data
       });
-      console.log(this.state);
+      // console.log(this.state);
     });
   };
 
   getUpcomingShowsFromDb = () => {
     axios.get('http://localhost:8000/api/upcomingshows/').then(response => {
-      console.log(response);
+      // console.log(response);
       this.setState({
         upcomingShows: response.data
       });
-      console.log(this.state);
+      // console.log(this.state);
     });
+  };
+
+  deleteUpcomingShow = index => {
+    axios.delete('http://localhost:8000/api/upcomingshows/' + index);
+    console.log('Called deleteUpcomingShow');
   };
 
   render() {
@@ -43,7 +49,10 @@ class App extends React.Component {
       <div>
         <h1>HELLO FROM APP.JS</h1>
         <StorySubmission storysubmission={this.state.storysubmission} />
-        <UpcomingShows upcomingshows={this.state.upcomingShows} />
+        <UpcomingShows
+          deleteupcomingshow={this.deleteUpcomingShow}
+          upcomingshows={this.state.upcomingShows}
+        />
       </div>
     );
   }
--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 class StorySubmission extends React.Component {
+  deleteUpcomingShow = index => {
+    this.props.deleteupcomingshow(index);
+  };
+
   render() {
     const listToDisplay = this.props.upcomingshows.map((show, index) => {
       // the return acts like a .push so everytime I return something it is pushed into listToDisplay
       return (
         <li className="item" key={index}>
-          <button>Delete Upcoming show</button>
+          <button onClick={() => this.deleteUpcomingShow(show._id)}>
+            Delete Upcoming show
+          </button>
           {show.posterImageLink}
         </li>
       );
