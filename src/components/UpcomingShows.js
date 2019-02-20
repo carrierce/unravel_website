53c52e306d9286cfb1d217ef33089a3c6600c57a
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-20 17:46:56 +0800

    getting state ready to submit

--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class StorySubmission extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posterImageLink: '',
+      showDate: '',
+      venue: '',
+      showBlurb: '',
+      ticketUrl: ''
+    };
+  }
   deleteUpcomingShow = index => {
     this.props.deleteupcomingshow(index);
+  };
+
+  formChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+    // console.log(this.state);
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+    console.log(this.state);
   };
 
   render() {
@@ -20,6 +41,41 @@ class StorySubmission extends React.Component {
     return (
       <div>
         <h1>Hello from UpcomingShows!</h1>
+        <form onSubmit={() => this.submitForm()}>
+          <h5>Submit Upcoming Show Data</h5>
+          <input
+            id="posterImageLink"
+            value={this.state.posterImageLink}
+            onChange={e => this.formChange(e)}
+            placeholder="poster image URL"
+          />
+          <input
+            type="date"
+            id="showDate"
+            value={this.state.showDate}
+            onChange={e => this.formChange(e)}
+            placeholder="show date"
+          />
+          <input
+            id="venue"
+            value={this.state.venue}
+            onChange={e => this.formChange(e)}
+            placeholder="venue address"
+          />
+          <input
+            id="showBlurb"
+            value={this.state.showBlurb}
+            onChange={e => this.formChange(e)}
+            placeholder="show blurb"
+          />
+          <input
+            id="ticketUrl"
+            value={this.state.ticketUrl}
+            onChange={e => this.formChange(e)}
+            placeholder="ticket purchase URL"
+          />
+          <button onClick={e => this.submitForm(e)}>Submit</button>
+        </form>
         <ul>{listToDisplay}</ul>
       </div>
     );
