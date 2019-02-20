a4c38b61144b2e8de742557f71e43350a6bf4047
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-20 14:22:05 +0800

    basic storysubmission display works

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import UpcomingShows from './UpcomingShows';
+import StorySubmission from './StorySubmission';
+import axios from 'axios';
 
-const App = () => {
-  return <UpcomingShows />;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDos: [],
+      storysubmission: []
+    };
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+  }
+
+  getDataFromDb = () => {
+    axios.get('http://localhost:8000/api/storysubmission/').then(response => {
+      console.log(typeof response.data);
+      this.setState({
+        storysubmission: response.data
+      });
+      console.log(this.state);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>HELLO FROM APP.JS</h1>
+        <StorySubmission storysubmission={this.state.storysubmission} />
+      </div>
+    );
+  }
+}
 
 export default App;
