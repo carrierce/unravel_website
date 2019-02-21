030169ecad0b6926ce48d2162f19a0c3c43c30a6
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-21 11:00:28 +0800

    working on editing shows

--- src/components/UpcomingShows.js
+++ src/components/UpcomingShows.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import UpcomingShowItem from './UpcomingShowItem';
 
-class StorySubmission extends React.Component {
+class UpcomingShows extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -24,6 +25,11 @@ class StorySubmission extends React.Component {
     this.props.postupcomingshow(this.state);
   };
 
+  moveDataToEditUpcomingShow = show => {
+    console.log(show);
+    return show;
+  };
+
   render() {
     const listToDisplay = this.props.upcomingshows.map((show, index) => {
       // the return acts like a .push so everytime I return something it is pushed into listToDisplay
@@ -31,6 +37,9 @@ class StorySubmission extends React.Component {
         <li className="item" key={index}>
           <button onClick={() => this.deleteUpcomingShow(show._id)}>
             Delete Upcoming show
+          </button>
+          <button onClick={() => this.moveDataToEditUpcomingShow(show)}>
+            Edit Upcoming Show
           </button>
           {show.posterImageLink}
         </li>
@@ -77,10 +86,50 @@ class StorySubmission extends React.Component {
           />
           <button onClick={e => this.submitForm(e)}>Submit</button>
         </form>
+        <br />
+        <br />
+        <form onSubmit={() => this.submitForm()}>
+          <h5>
+            Edit Upcoming Show Data (poster image url needs to be at least 5
+            characters longs)
+          </h5>
+          <input
+            id="posterImageLink"
+            value={this.moveDataToEditUpcomingShow.posterImageLink}
+            // onChange={e => this.formChange(e)}
+            placeholder="poster image URL"
+          />
+          <input
+            type="date"
+            id="showDate"
+            value={this.moveDataToEditUpcomingShow.showDate}
+            // onChange={e => this.formChange(e)}
+            placeholder="show date"
+          />
+          <input
+            id="venue"
+            value={this.moveDataToEditUpcomingShow.venue}
+            // onChange={e => this.formChange(e)}
+            placeholder="venue address"
+          />
+          <input
+            id="showBlurb"
+            value={this.moveDataToEditUpcomingShow.showBlurb}
+            // onChange={e => this.formChange(e)}
+            placeholder="show blurb"
+          />
+          <input
+            id="ticketUrl"
+            value={this.moveDataToEditUpcomingShow.ticketUrl}
+            // onChange={e => this.formChange(e)}
+            placeholder="ticket purchase URL"
+          />
+          {/* <button onClick={e => this.submitForm(e)}>Submit</button> */}
+        </form>
         <ul>{listToDisplay}</ul>
       </div>
     );
   }
 }
 
-export default StorySubmission;
+export default UpcomingShows;
