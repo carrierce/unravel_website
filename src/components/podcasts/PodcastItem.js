95ba7ef8f0dc5f73d376fa36a9e39d8228b907e1
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-25 10:51:30 +0800

    converting podcast item to a class based component

--- src/components/podcasts/PodcastItem.js
+++ src/components/podcasts/PodcastItem.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import EditPodcast from './EditPodcast';
 
-const PodcastItem = props => {
-  return (
-    <div key={props.index}>
-      <div className="ui item">
-        <h2>Podcast Data</h2>
-        <p>Cover Image Link: {props.podcast.podcastCoverImageLink}</p>
-        <p>Name: {props.podcast.podcastName}</p>
-        <p>Blurb: {props.podcast.podcastBlurb}</p>
-        <p>Show Notes: {props.podcast.podcastShowNotes}</p>
-        <p>Embed Link: {props.podcast.podcastEmbedLink}</p>
+class PodcastItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div key={this.props.index}>
+        <div className="ui item">
+          <h2>Podcast Data</h2>
+          <p>Cover Image Link: {this.props.podcast.podcastCoverImageLink}</p>
+          <p>Name: {this.props.podcast.podcastName}</p>
+          <p>Blurb: {this.props.podcast.podcastBlurb}</p>
+          <p>Show Notes: {this.props.podcast.podcastShowNotes}</p>
+          <p>Embed Link: {this.props.podcast.podcastEmbedLink}</p>
+        </div>
+        <br />
+        <button className="positive ui button">Edit Podcast</button>
+        <button
+          className="negative ui button"
+          onClick={() => this.props.deletepodcast(this.state._id)}
+        >
+          Delete Podcast
+        </button>
+        <div className="ui divider" />
+        <EditPodcast
+          deletepodcast={this.props.deletepodcast}
+          editpodcast={this.props.editpodcast}
+          podcast={this.props.podcast}
+        />
       </div>
-      <br />
-      <button className="positive ui button">Edit Podcast</button>
-      <button
-        className="negative ui button"
-        onClick={() => this.props.deletepodcast(this.state._id)}
-      >
-        Delete Podcast
-      </button>
-      <div className="ui divider" />
-      <EditPodcast
-        deletepodcast={props.deletepodcast}
-        editpodcast={props.editpodcast}
-        podcast={props.podcast}
-      />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PodcastItem;
