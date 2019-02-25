966e1259cfd3bbeeb04f9593b7f1bf348ca7f080
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-25 12:31:06 +0800

    getting podcasts to appear in reverse order

--- src/components/podcasts/Podcasts.js
+++ src/components/podcasts/Podcasts.js
@@ -8,20 +8,23 @@ class Podcasts extends React.Component {
   };
 
   render() {
-    const renderedList = this.props.podcasts.map((podcast, index) => {
-      console.log(podcast);
-      return (
-        <div key={index} className="ui segment">
-          <PodcastItem
-            className="ui items"
-            editpodcast={this.props.editpodcast}
-            podcast={podcast}
-            index={index}
-            deletepodcast={this.deletePodcast}
-          />
-        </div>
-      );
-    });
+    const renderedList = this.props.podcasts
+      .slice(0)
+      .reverse()
+      .map((podcast, index) => {
+        console.log(podcast);
+        return (
+          <div key={index} className="ui segment">
+            <PodcastItem
+              className="ui items"
+              editpodcast={this.props.editpodcast}
+              podcast={podcast}
+              index={index}
+              deletepodcast={this.deletePodcast}
+            />
+          </div>
+        );
+      });
     return (
       <div>
         <CreatePodcast postpodcast={this.props.postpodcast} />
