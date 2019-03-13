e6a2deeec0022c8aa46af6291abbc7e06e4675fb
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-13 10:57:10 +0800

    finishing the mobile tell your story title

--- src/components/mobile/popupnav.js
+++ src/components/mobile/popupnav.js
@@ -21,7 +21,7 @@ class PopUpNav extends React.Component {
         <div id="popUpLinks" className="ui grid">
           <div className="row">
             <h3 onClick={this.props.closeSideBar}>
-              <Link to="/">Unravel Live</Link>
+              <Link to="/mobile/home">Unravel Live</Link>
             </h3>
           </div>
           <div id="popUpIndented" className="row">
