e26b16d79b7229e1644233dd5dac3a6584d2edb5
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-14 14:22:31 +0800

    getting the api navbar mostly working

--- src/components/api/apinavbar.js
+++ src/components/api/apinavbar.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { Menu, Dropdown } from 'semantic-ui-react';
 
 class ApiNavBar extends Component {
   state = { open: false };
 
   render() {
     return (
-      <div className="ui inverted menu">
-        <div className="middle aligned left menu">
-          <h2 className="item">Unravel CMS</h2>
-        </div>
-        <ul className="right menu">
-          <li className="item">
-            <Link to="/api/podcasts">Podcasts</Link>
-          </li>
-          <li className="item">
-            <Link to="/api/upcomingshows">Upcoming Shows</Link>
-          </li>
-          <li className="item">
-            <Link to="/api/pastshows">Past Shows</Link>
-          </li>
-          <li className="item">
-            <Link to="/api/storysubmissions">Story Submission</Link>
-          </li>
-          <li className="item">
-            <Link to="/api/impact">Impact</Link>
-          </li>
-        </ul>
-      </div>
+      <Menu size="massive" borderless inverted>
+        <Menu.Item header>Unravel Content Management</Menu.Item>
+        <Dropdown
+          item
+          className="right floated column"
+          id="apiDropDownMenu"
+          text="Sections"
+          position="right"
+        >
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Link to="/api/podcasts">Podcasts</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/api/upcomingshows">Upcoming Shows</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/api/pastshows">Past Shows</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/api/storysubmissions">Story Submission</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/api/impact">Impact</Link>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
     );
   }
 }
