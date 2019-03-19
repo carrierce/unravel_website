c44c5b08c37b11b8e5d2b64ec62130bacf2b6529
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-19 15:30:50 +0800

    adding front end validation to story submission

--- src/components/mobile/tellyourstory/MobileTellYourStorySubmissionForm.js
+++ src/components/mobile/tellyourstory/MobileTellYourStorySubmissionForm.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  Object.values(rest).forEach(val => {
+    val === '' && (valid = false);
+  });
+
+  return valid;
+};
 class MobileTellYourStorySubmissionForm extends React.Component {
   constructor(props) {
     super(props);
@@ -8,37 +25,62 @@ class MobileTellYourStorySubmissionForm extends React.Component {
       email: '',
       story: '',
       questionOrComment: '',
-      error: false,
-      message: ''
+      formErrors: {
+        name: '',
+        email: '',
+        story: '',
+        questionOrComment: ''
+      },
+      submitError: false
     };
     this.initialState = {
       name: '',
       email: '',
       story: '',
-      questionOrComment: ''
+      questionOrComment: '',
+      formErrors: {
+        name: '',
+        email: '',
+        story: '',
+        questionOrComment: ''
+      },
+      submitError: false
     };
   }
 
   formChange = e => {
-    let val = e.target.id;
-    console.log(this.state[val].length);
-
+    const { id, value } = e.target;
+    let formErrors = this.state.formErrors;
+    switch (id) {
+      case 'name':
+        formErrors.name = value.length < 1 ? 'Name cannot be blank' : '';
+        break;
+      case 'email':
+        formErrors.email = emailRegex.test(value)
+          ? ''
+          : 'invalid email address';
+        break;
+      case 'story':
+        formErrors.story = value.length < 1 ? 'Story cannot be blank' : '';
+      default:
+        break;
+    }
     this.setState({ [e.target.id]: e.target.value });
   };
 
   submitForm = e => {
     e.preventDefault();
-    if (this.state.name.length <= 0) {
-      this.setState({ error: true, message: 'there is an error' });
-      return;
+    if (formValid(this.state)) {
+      console.log('hello from mobile tell yoru story submission');
+      this.props.poststorysubmission(this.state);
+      this.setState(this.initialState);
+    } else {
+      this.setState({ submitError: true });
     }
-    this.props.poststorysubmission(this.state);
-    this.setState(this.initialState);
   };
 
   render() {
-    console.log(this.props);
-
+    const { formErrors } = this.state;
     return (
       <div id="mobileTellYourStoryStorySubmission" className="ui centered grid">
         <h2 className="row">Submission Form</h2>
@@ -51,17 +93,20 @@ class MobileTellYourStorySubmissionForm extends React.Component {
                 onChange={e => this.formChange(e)}
                 placeholder="Name..."
               />
-              <span>{this.state.name.message}</span>
+              {formErrors.name.length > 0 && (
+                <span id="errorMessage">{formErrors.name}</span>
+              )}
             </div>
             <div className="field">
               <input
-                name="email"
-                type="email"
                 id="email"
                 value={this.state.email}
                 onChange={e => this.formChange(e)}
                 placeholder="Email..."
               />
+              {formErrors.email.length > 0 && (
+                <span id="errorMessage">{formErrors.email}</span>
+              )}
             </div>
             <div className="field">
               <textarea
@@ -71,6 +116,9 @@ class MobileTellYourStorySubmissionForm extends React.Component {
                 onChange={e => this.formChange(e)}
                 placeholder="Story..."
               />
+              {formErrors.story.length > 0 && (
+                <span id="errorMessage">{formErrors.story}</span>
+              )}
             </div>
             <div className="field">
               <textarea
