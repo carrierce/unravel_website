026dc8de9fda1a437deba4d3249d53ea9e7b84be
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-20 13:51:29 +0800

    starting work on podcast episode expander

--- src/components/mobile/podcast/PodcastEpisodes.js
+++ src/components/mobile/podcast/PodcastEpisodes.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import { DocumentProvider } from 'mongoose';
-const moment = require('moment');
 const crowd = require('../../../crowd.jpg');
 
+let filteredList = [];
+let renderedList = [];
 class PodcastEpisodes extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  showMoreEpisodes = () => {
+    console.log(filteredList);
+  };
+
   render() {
-    console.log(this.props.podcasts);
-    const renderedList = this.props.podcasts
-      .slice(-3)
-      .reverse()
-      .map((podcast, index) => {
-        return (
-          <div className="ui grid" key={index}>
-            <h3 id="podcastsEpisodeName" className="row">
-              {podcast.podcastName}
-            </h3>
-            <div className="row">
-              <img id="podcastEmbedCroppedWidth" src={crowd} />
-            </div>
-            <p id="podcastsEpisodeBlurb" className="row">
-              {podcast.podcastBlurb}
-            </p>
-            <div id="podcastsPlayer" className="row">
-              <img src="https://via.placeholder.com/550x100" />
-            </div>
-            <h3 id="podcastsShowNotesTitle">Show Notes</h3>
-            <p>{podcast.podcastShowNotes}</p>
+    filteredList = this.props.podcasts.slice(-1);
+    renderedList = filteredList.reverse().map((podcast, index) => {
+      return (
+        <div className="ui grid" key={index}>
+          <h3 id="podcastsEpisodeName" className="row">
+            {podcast.podcastName}
+          </h3>
+          <div className="row">
+            <img id="podcastEmbedCroppedWidth" src={crowd} />
           </div>
-        );
-      });
+          <p id="podcastsEpisodeBlurb" className="row">
+            {podcast.podcastBlurb}
+          </p>
+          <div id="podcastsPlayer" className="row">
+            <img src="https://via.placeholder.com/550x100" />
+          </div>
+          <h3 id="podcastsShowNotesTitle">Show Notes</h3>
+          <p>{podcast.podcastShowNotes}</p>
+        </div>
+      );
+    });
     return (
       <div id="podcastsContainer" className="ui grid">
         <h1 id="podcastsLatestEpisodes" className="row">
@@ -43,6 +44,7 @@ class PodcastEpisodes extends React.Component {
           className="row"
           className="ui button"
           id="podcastsShowMorePodcastsButton"
+          onClick={this.showMoreEpisodes}
         >
           Load more episodes
         </button>
